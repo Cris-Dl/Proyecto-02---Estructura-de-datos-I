b695d88eccea12157d84df14cfa373cb84a0f79a
Cristhian de León <deleonperezcristhianestuardo@gmail.com>
2026-05-13 19:52:08 -0600

    se añadió la opción de elegir el tipo de árbol según quiera el usuario

--- logica.py
+++ logica.py
@@ -8,11 +8,43 @@ class Nodo:
         self.derecho = None
         self.x = 0
         self.y = 0
+        self.altura = 1
 
 class ArbolBinario:
     def __init__(self):
         self.raiz = None
 
+    def insertar(self, valor):
+        if self.raiz is None:
+            self.raiz = Nodo(valor)
+            return
+        cola = [self.raiz]
+        while cola:
+            nodo = cola.pop(0)
+            if nodo.izquierdo is None:
+                nodo.izquierdo = Nodo(valor)
+                break
+            else:
+                cola.append(nodo.izquierdo)
+
+            if nodo.derecho is None:
+                nodo.derecho = Nodo(valor)
+                break
+            else:
+                cola.append(nodo.derecho)
+
+    def calcular_posiciones(self, ancho_lienzo):
+        if self.raiz is not None:
+            self._calcular_coordenadas(self.raiz, 0, ancho_lienzo / 2, 50, ancho_lienzo / 4, 80)
+
+    def _calcular_coordenadas(self, nodo, nivel, x, y, desplazamiento, espaciado_y):
+        if nodo is not None:
+            nodo.x = x
+            nodo.y = y
+            self._calcular_coordenadas(nodo.izquierdo, nivel + 1, x - desplazamiento, y + espaciado_y, desplazamiento / 2, espaciado_y)
+            self._calcular_coordenadas(nodo.derecho, nivel + 1, x + desplazamiento, y + espaciado_y, desplazamiento / 2, espaciado_y)
+
+class ArbolBST(ArbolBinario):
     def insertar(self, valor):
         if self.raiz is None:
             self.raiz = Nodo(valor)
@@ -31,23 +63,15 @@ class ArbolBinario:
             else:
                 self._insertar_recursivo(nodo.derecho, valor)
 
-    def calcular_posiciones(self, ancho_lienzo):
-        if self.raiz is not None:
-            self._calcular_coordenadas(self.raiz, 0, ancho_lienzo / 2, 50, ancho_lienzo / 4, 80)
+class ArbolAVL(ArbolBST):
+    def insertar(self, valor):
+        super().insertar(valor)
 
-    def _calcular_coordenadas(self, nodo, nivel, x, y, desplazamiento, espaciado_y):
-        if nodo is not None:
-            nodo.x = x
-            nodo.y = y
-            self._calcular_coordenadas(nodo.izquierdo, nivel + 1, x - desplazamiento, y + espaciado_y, desplazamiento / 2, espaciado_y) # Hijo izquierdo: se mueve a la izquierda
-            self._calcular_coordenadas(nodo.derecho, nivel + 1, x + desplazamiento, y + espaciado_y, desplazamiento / 2, espaciado_y) # Hijo derecho: se mueve a la derecha
-
-#Interfaz
 def main(page: ft.Page):
     page.title = "Visualizador de Árboles - Proyecto 2"
     page.theme_mode = ft.ThemeMode.DARK
     page.padding = 20
-    mi_arbol = ArbolBinario()
+    mi_arbol = ArbolBST()
     lienzo = cv.Canvas(width=800, height=600, expand=True)
 
     def dibujar_arbol_en_lienzo(nodo):
@@ -61,6 +85,18 @@ def main(page: ft.Page):
             lienzo.shapes.append(cv.Circle(nodo.x, nodo.y, 20, paint=ft.Paint(color=ft.Colors.BLUE_700)))
             lienzo.shapes.append(cv.Text(nodo.x - 10, nodo.y - 10, str(nodo.valor), style=ft.TextStyle(size=16, weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE)))
 
+    def cambiar_tipo_arbol(e):
+        nonlocal mi_arbol
+        tipo = selector_arbol.value
+        if tipo == "Binario":
+            mi_arbol = ArbolBinario()
+        elif tipo == "BST":
+            mi_arbol = ArbolBST()
+        elif tipo == "AVL":
+            mi_arbol = ArbolAVL()
+        lienzo.shapes.clear()
+        page.update()
+
     async def btn_insertar_click(e):
         try:
             valor = int(txt_valor.value)
@@ -73,9 +109,15 @@ def main(page: ft.Page):
             page.update()
         except ValueError:
             pass
-    txt_valor = ft.TextField(label="Valor del Nodo", width=150)
-    btn_insertar = ft.Button("Insertar", on_click=btn_insertar_click)
-    panel_control = ft.Column([ft.Text("Controles", size=20, weight=ft.FontWeight.BOLD), txt_valor, btn_insertar], width=200)
-    page.add(ft.Row([panel_control, ft.Container(content=lienzo, bgcolor=ft.Colors.BLACK87, border_radius=10, expand=True)], expand=True))
+    selector_arbol = ft.Dropdown(
+        label="Tipo de Árbol",
+        options=[ft.dropdown.Option("Binario"), ft.dropdown.Option("BST"), ft.dropdown.Option("AVL"),],
+        value="BST",
+        width=180,
+        on_select=cambiar_tipo_arbol)
+    txt_valor = ft.TextField(label="Valor del Nodo", width=180)
+    btn_insertar = ft.Button("Insertar", on_click=btn_insertar_click, width=180)
+    panel_control = ft.Column([ft.Text("Controles", size=20, weight=ft.FontWeight.BOLD), selector_arbol, txt_valor, btn_insertar], width=200)
+    page.add(ft.Row([panel_control,ft.Container(content=lienzo, bgcolor=ft.Colors.BLACK87, border_radius=10, expand=True)], expand=True))
 
 ft.run(main)
